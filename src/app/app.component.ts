95dc2f3a66586c45322a6da22ef1b5026ea2f2aa
Jared Win Eaton <jrod454@gmail.com>
2019-01-30 01:50:57 -0600

    Completed the simple functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Poomsae';
+
+  heightJump: number = 0;
+  numJumpKicks: number = 0;
+  turnGradient: number = 0;
+  kickPerformance: number = 0;
+  acrobatic: number = 0;
+  basicMovement: number = 0;
+  creativeness: number = 0;
+  harmony: number = 0;
+  energyExp: number = 0;
+  music: number = 0;
+
+  deductions: number = 0;
+
+  techScore() {
+    return this.heightJump + this.numJumpKicks + this.turnGradient + this.kickPerformance + this.acrobatic + this.basicMovement;
+  }
+
+  presScore() {
+    return this.creativeness + this.harmony + this.energyExp + this.music;
+  }
+
+  totalScore() {
+    return this.techScore() + this.presScore() + this.deductions;
+  }
 }
